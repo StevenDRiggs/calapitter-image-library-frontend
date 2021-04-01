6b07cf8ccf908916f80889c9770d3a5d8e09d1c2
StevenDRiggs <sfriggs21@gmail.com>
2021-04-01 07:20:36 -0600

    animated caterpillar

--- animations/scripts/caterpillar.js
+++ animations/scripts/caterpillar.js
@@ -1,49 +1,57 @@
 import { gsap } from 'gsap'
 
 
-const clearSVG = () => {
-  const tl = gsap.timeline({id: 'totalBounceIn'})
-
-  tl.addLabel('start')
-    .to('.pageFooter', {opacity: 0, duration: 0}, 'start')
-    .to('#tail3', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#tail2', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#tail1', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#body3', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#body2', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#body1', {x: 150, y: -500, duration: 0}, 'start')
-    .to('#head', {x: 150, y: -500, duration: 0}, 'start')
-}
-
-const segmentBounceIn = segment => {
-  const segmentTL = gsap.timeline({id: `${segment}BounceIn`})
-
-  segmentTL.addLabel(`${segment}Bounce1fall`)
-    .to(segment, {x: -150, duration: 3.5}, `${segment}Bounce1fall`)
-    .to(segment, {y: 475, duration: 0.5}, `${segment}Bounce1fall`)
-    .addLabel(`${segment}Bounce2rise`, '-=3')
-    .to(segment, {y: 425, duration: 0.5, ease: 'power2.out'}, `${segment}Bounce2rise`)
-    .addLabel(`${segment}Bounce2fall`, '-=2.5')
-    .to(segment, {y: 475, duration: 0.5, ease: 'power2.in'}, `${segment}Bounce2fall`)
-    .addLabel(`${segment}Bounce3rise`, '-=2')
-    .to(segment, {y: 450, duration: 0.5, ease: 'power2.out'}, `${segment}Bounce3rise`)
-    .addLabel(`${segment}Bounce3fall`, '-=1.5')
-    .to(segment, {y: 475, duration: 0.5, ease: 'power2.in'}, `${segment}Bounce3fall`)
-    .addLabel(`${segment}Bounce4rise`, '-=1')
-    .to(segment, {y: 462.5, duration: 0.5, ease: 'power2.out'}, `${segment}Bounce4rise`)
-    .addLabel(`${segment}Bounce4fall`, '-=0.5')
-    .to(segment, {y: 475, duration: 0.5, ease: 'power2.in'}, `${segment}Bounce4fall`)
-}
-
 const bounceIn = () => {
-  segmentBounceIn('#head-head')
-  segmentBounceIn('#body1-body')
-  segmentBounceIn('#body2-body')
-  segmentBounceIn('#body3-body')
-  segmentBounceIn('#tail1-body')
-  segmentBounceIn('#tail2')
-  segmentBounceIn('#tail3')
+  const tl = gsap.timeline({id: 'bounceIn'})
+
+  tl.addLabel('prep')
+    .to('#head-face', {opacity: 0, duration: 0}, 'prep')
+    .to('#head-antennae', {opacity: 0, duration: 0}, 'prep')
+    .to('#body1-legs', {opacity: 0, duration: 0}, 'prep')
+    .to('#body2-legs', {opacity: 0, duration: 0}, 'prep')
+    .to('#body3-legs', {opacity: 0, duration: 0}, 'prep')
+    .to('#tail1-legs', {opacity: 0, duration: 0}, 'prep')
+    .to('#head-head', {opacity: 0, duration: 0}, 'prep')
+    .to('#body1-body', {opacity: 0, duration: 0}, 'prep')
+    .to('#body2-body', {opacity: 0, duration: 0}, 'prep')
+    .to('#body3-body', {opacity: 0, duration: 0}, 'prep')
+    .to('#tail1-body', {opacity: 0, duration: 0}, 'prep')
+    .to('#tail2', {opacity: 0, duration: 0}, 'prep')
+    .to('#tail3', {opacity: 0, duration: 0}, 'prep')
+
+  tl.addLabel('bounce')
+    .to('.segment', {x: -150, y: -200, duration: 0}, 'bounce')
+    .to('.segment', {opacity: 1, duration: 0, stagger: 0.4}, 'bounce')
+    .to('.segment', {x: 0, duration: 3.5, stagger: 0.4}, 'bounce')
+    .to('.segment', {y: 0, duration: 0.5, stagger: 0.4, ease: 'power2.in'}, 'bounce')
+    .to('.segment', {y: -100, duration: 0.5, stagger: 0.4, ease: 'power2.out'}, 'bounce+=0.5')
+    .to('.segment', {y: 0, duration: 0.5, stagger: 0.4, ease: 'power2.in'}, 'bounce+=1')
+    .to('.segment', {y: -50, duration: 0.5, stagger: 0.4, ease: 'power2.out'}, 'bounce+=1.5')
+    .to('.segment', {y: 0, duration: 0.5, stagger: 0.4, ease: 'power2.in'}, 'bounce+=2')
+    .to('.segment', {y: -25, duration: 0.5, stagger: 0.4, ease: 'power2.out'}, 'bounce+=2.5')
+    .to('.segment', {y: 0, duration: 0.5, stagger: 0.4, ease: 'power2.in'}, 'bounce+=3')
+
+  tl.addLabel('fadeIn')
+    .to('#body1-legs', {opacity: 1, duration: 0.5}, 'fadeIn')
+    .to('#body2-legs', {opacity: 1, duration: 0.5}, 'fadeIn+=0.1')
+    .to('#body3-legs', {opacity: 1, duration: 0.5}, 'fadeIn+=0.2')
+    .to('#tail1-legs', {opacity: 1, duration: 0.5}, 'fadeIn+=0.3')
+    .to('#head-face-mouth', {opacity: 0, duration: 0}, 'fadeIn')
+    .to('#head-face', {opacity: 1, duration: 1}, 'fadeIn+=0.6')
+    .to('#head-antennae', {y: 25, duration: 0}, 'fadeIn+=1')
+    .to('#head-antennae', {opacity: 1, y: 0, duration: 0.3}, 'fadeIn+=1.1')
+    .to('#head-face-mouth', {opacity: 1, duration: 1}, 'fadeIn+=1.6')
+    .then(blink())
+}
+
+const blink = () => {
+  const tl = gsap.timeline({id: 'blink', repeat: -1})
+
+  tl.to('#eyes-clip-path', {y: 20, duration: 0.1})
+    .to('#eyes-clip-path', {y: 0, duration: 0.1})
+
+  tl.repeatDelay(1.8)
 }
 
 
-export { clearSVG, segmentBounceIn }
+export { bounceIn }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
 
 //import Chrysalis from '../images/logoReact.svg'
-import { clearSVG, segmentBounceIn } from '../animations/scripts/caterpillar'
+import { bounceIn } from '../animations/scripts/caterpillar'
 
 import styles from '../styles/Home.module.css'
 
@@ -18,27 +18,35 @@ class Home extends Component {
       </defs>
       <circle id="tail3" className='segment' cx="250" cy="150" r="20" fill="violet"></circle>
       <circle id="tail2" className='segment' cx="230" cy="160" r="30" fill="indigo"></circle>
-      <g id="tail1" className='segment'>
-        <line id="tail1-leg-left" x1="190" y1="157" x2="190" y2="225" stroke="black" strokeWidth="5"></line>
-        <line id="tail1-leg-right" x1="210" y1="157" x2="210" y2="225" stroke="black" strokeWidth="5"></line>
-        <circle id="tail1-body" cx="200" cy="157" r="40" fill="blue"></circle>
+      <g id="tail1">
+        <g id="tail1-legs">
+          <line id="tail1-legs-left" x1="190" y1="157" x2="190" y2="225" stroke="black" strokeWidth="5"></line>
+          <line id="tail1-legs-right" x1="210" y1="157" x2="210" y2="225" stroke="black" strokeWidth="5"></line>
+        </g>
+        <circle id="tail1-body" className='segment' cx="200" cy="157" r="40" fill="blue"></circle>
       </g>
-      <g id="body3" className='segment'>
-        <line id="body3-leg-left" x1="165" y1="165" x2="165" y2="240" stroke="black" strokeWidth="5"></line>
-        <line id="body3-leg-right" x1="180" y1="165" x2="180" y2="240" stroke="black" strokeWidth="5"></line>
-        <circle id="body3-body" cx="170" cy="165" r="50" fill="green"></circle>
+      <g id="body3">
+        <g id="body3-legs">
+          <line id="body3-legs-left" x1="165" y1="165" x2="165" y2="240" stroke="black" strokeWidth="5"></line>
+          <line id="body3-legs-right" x1="180" y1="165" x2="180" y2="240" stroke="black" strokeWidth="5"></line>
+        </g>
+        <circle id="body3-body" className='segment' cx="170" cy="165" r="50" fill="green"></circle>
       </g>
-      <g id="body2" className='segment'>
-        <line id="body2-leg-left" x1="140" y1="155" x2="140" y2="230" stroke="black" strokeWidth="5"></line>
-        <line id="body2-leg-right" x1="160" y1="155" x2="160" y2="230" stroke="black" strokeWidth="5"></line>
-        <circle id="body2-body" cx="150" cy="155" r="50" fill="yellow"></circle>
+      <g id="body2">
+        <g id="body2-legs">
+          <line id="body2-legs-left" x1="140" y1="155" x2="140" y2="230" stroke="black" strokeWidth="5"></line>
+          <line id="body2-legs-right" x1="160" y1="155" x2="160" y2="230" stroke="black" strokeWidth="5"></line>
+        </g>
+        <circle id="body2-body" className='segment' cx="150" cy="155" r="50" fill="yellow"></circle>
       </g>
-      <g id="body1" className='segment'>
-        <line id="body1-leg-left" x1="110" y1="160" x2="110" y2="235" stroke="black" strokeWidth="5"></line>
-        <line id="body1-leg-right" x1="130" y1="160" x2="130" y2="235" stroke="black" strokeWidth="5"></line>
-        <circle id="body1-body" cx="120" cy="160" r="50" fill="orange"></circle>
+      <g id="body1">
+        <g id='body1-legs'>
+          <line id="body1-legs-left" x1="110" y1="160" x2="110" y2="235" stroke="black" strokeWidth="5"></line>
+          <line id="body1-legs-right" x1="130" y1="160" x2="130" y2="235" stroke="black" strokeWidth="5"></line>
+        </g>
+        <circle id="body1-body" className='segment' cx="120" cy="160" r="50" fill="orange"></circle>
       </g>
-      <g id="head" className='segment'>
+      <g id="head">
         <g id="head-antennae">
           <g id="head-antennae-left">
             <path id="head-antennae-left-stamen" stroke="black" strokeWidth="5" fill="none" d="M 90,115 C 100,100 100,100 80,80"></path>
@@ -49,7 +57,7 @@ class Home extends Component {
             <circle id="head-antennae-right-pistil" cx="110" cy="80" r="10" stroke="black" strokeWidth="5" fill="gray"></circle>
           </g>
         </g>
-        <circle id="head-head" cx="100" cy="150" r="50" fill="red"></circle>
+        <circle id="head-head" className='segment' cx="100" cy="150" r="50" fill="red"></circle>
         <g id="head-face">
           <g id="head-face-eyes" clip-path="url(#eyes-clip-path)">
             <circle id="head-face-eyes-left" cx="80" cy="140" r="10" fill="white"></circle>
@@ -64,14 +72,7 @@ class Home extends Component {
       const targetElement = document.querySelector('#SVGs')
       disableBodyScroll(targetElement)
 
-      clearSVG()
-      segmentBounceIn('#head-head')
-      segmentBounceIn('#body1-body')
-      segmentBounceIn('#body2-body')
-      segmentBounceIn('#body3-body')
-      segmentBounceIn('#tail1-body')
-      segmentBounceIn('#tail2')
-      segmentBounceIn('#tail3')
+      bounceIn()
     }
 
   componentWillUnmount() {
